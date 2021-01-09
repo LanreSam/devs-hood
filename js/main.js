4b7e88f365ef345f7bb11556d75bea1f5f6ea376
LanreSam <ezechelanre@gmail.com>
2021-01-09 17:12:23 +0100

    still adding team mates

--- js/main.js
+++ js/main.js
@@ -93,10 +93,10 @@
   });
 
   // jQuery counterUp (used in Whu Us section)
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
+  // $('[data-toggle="counter-up"]').counterUp({
+  //   delay: 10,
+  //   time: 1000
+  // });
 
   // Porfolio isotope and filter
   $(window).on('load', function () {
