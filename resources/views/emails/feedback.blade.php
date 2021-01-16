37104d95b868e72787cc6fce08931605a6e70591
LanreSam <ezechelanre@gmail.com>
2021-01-16 01:04:50 +0100

    making changes

--- resources/views/emails/feedback.blade.php
+++ resources/views/emails/feedback.blade.php
@@ -6,15 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Feedback from contact form</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
     <h2>Thanks for contacting us</h2>
     <p>We will reach out to you shortly</p>
-    <button class="btn btn-dark"><a href="/" class="text-white">Visit Website</a></button>
+    <a href="https://devshood.herokuapp.com" class="text-white">Visit Website</a>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 </html>
