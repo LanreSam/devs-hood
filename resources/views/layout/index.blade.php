fd4b0ec70e43978ba86db4d7a744c7930f1286b2
LanreSam <ezechelanre@gmail.com>
2021-02-02 16:39:27 +0100

    changed a few lines of code

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -80,15 +80,16 @@
   <section id="intro" class="clearfix">
     <div class="container d-flex h-100">
       <div class="row justify-content-center align-self-center">
-        <div class="col-md-6 intro-img order-md-last order-first">
-            <img src="{{ asset('assets/img/intro-img.png') }}" alt="" class="img-fluid">
-        </div>
 
         <div class="col-md-6 intro-info order-md-first order-last">
           <h2>DevsHood<br>Team of Computer <span>Scientist!</span></h2>
           <div>
             <a href="#about" class="btn-get-started scrollto">Get Started</a>
           </div>
+        </div>
+
+        <div class="col-md-6 intro-img order-md-last order-first">
+            <img src="{{ asset('assets/img/intro-img.png') }}" alt="" class="img-fluid">
         </div>
       </div>
 
