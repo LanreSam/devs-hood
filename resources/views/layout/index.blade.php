493904fcf6c9aa864f909f1002e8d7a5e31b06b4
LanreSam <ezechelanre@gmail.com>
2021-02-02 16:31:44 +0100

    changed a few lines of code

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -3,14 +3,13 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>DevsHood</title>
+  <title>DevsHood - Team of Computer Scientist</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
 
   <!-- Favicons -->
   <link href="{{ asset('assets/img/devs-hood.png') }}" rel="icon">
-  {{-- <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon"> --}}
 
   <!-- Google Fonts -->
   <link href="{{ asset('assets/https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,500,600,700,700i|Montserrat:300,400,500,600,700') }}" rel="stylesheet">
